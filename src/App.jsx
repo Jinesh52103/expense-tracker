fcd07b6eba373db6ccab92b1e0af980e898c8fd2
chirasvigowda <czg5634@psu.edu>
2025-04-21 21:57:37 -0400

    implemented some small changes to

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import Header from "./components/Header"
 import Form from "./components/Form"
 import BudgetSummary from "./components/BudgetSummary"
@@ -6,36 +6,39 @@ import IncomeInput from "./components/IncomeInput"
 import List from './components/List'
 
 function App() {
+    const [income, setIncome] = useState(0);
+    const [expenses, setExpenses] = useState([]);
 
-  const [income, setIncome] = useState(0);
-  const [expenses, setExpenses] = useState([])
+    const totalExpenses = expenses.reduce((total, expense) => {
+        return total + (expense.amount ? Number(expense.amount) : 0);
+    }, 0);
 
-  function handleDelete(index) {
-  const newData = expenses.filter((_, i) => i !== index);
-  setExpenses(newData);
-}
+    function handleDelete(index) {
+        const newData = expenses.filter((_, i) => i !== index);
+        setExpenses(newData);
+    }
 
-
-  return (
-    <div className="bg-gray-300 p-6 rounded-lg shadow-lg max-w-3xl mx-auto mt-10">
-      <Header />
-      <IncomeInput
-      setIncome={setIncome}
-      income={income}
-      />
-      <Form 
-      setData={setExpenses}
-      />
-      <BudgetSummary 
-      income={income}
-      />
-    {console.log("App is passing expenses to List:", expenses)}
-      <List 
-      data={expenses}
-      handleDelete={handleDelete}
-      />
-    </div>
-  )
+    return (
+        <div className="bg-gray-300 p-6 rounded-lg shadow-lg max-w-3xl mx-auto mt-10">
+            <Header />
+            <IncomeInput
+                setIncome={setIncome}
+                income={income}
+            />
+            <Form
+                setData={setExpenses}
+            />
+            <BudgetSummary
+                income={income}
+                expenses={totalExpenses}
+            />
+            {console.log("App is passing expenses to List:", expenses)}
+            <List
+                data={expenses}
+                handleDelete={handleDelete}
+            />
+        </div>
+    )
 }
 
 export default App
